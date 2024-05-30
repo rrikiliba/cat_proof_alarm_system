c1efa4687fee7bcd5b511760e06d8b91628a3485
Riccardo Libanora <riccardo.libanora@gmail.com>
2024-05-30 14:50:45 +0200

    added esp_32 code mock up

---
+++ esp_32/main.py
@@ -0,0 +1,47 @@
+import network
+import camera
+import utime
+import machine
+
+import secrets as cfg
+
+# connect to wifi
+wlan = network.WLAN()
+wlan.active(True)
+while not wlan.isconnected():
+    print('WIFI CONNECTING')
+    wlan.connect(cfg.WIFI_SSID, cfg.WIFI_PASSWORD)
+    utime.sleep_ms(1000)
+print('WIFI OK')
+
+camera.init(0, format=camera.JPEG, fb_location=camera.PSRAM)
+def recv_msg(self, topic, msg):
+    buf = camera.capture()
+    mqtt.publish(b'image/submit', buf)
+
+# keep trying to connect to mqtt indefinitely
+mqtt_ok = False
+while not mqtt_ok:
+    try:
+        mqtt = MQTTClient(client_id=cfg.DEVICE_ID,
+                            server=cfg.MQTT_BROKER,
+                            port=cfg.MQTT_PORT,
+                            user=cfg.MQTT_USER,
+                            password=cfg.MQTT_PASSWORD,
+                            keepalive=60,
+                            ssl=cfg.MQTT_SSL,
+                            ssl_params=cfg.MQTT_SSL_PARAMS)
+        mqtt.set_callback(recv_msg)
+        mqtt.connect()
+        mqtt_ok = True
+    except OSError as e:
+        print('MQTT CONNECTING')
+                
+# subscribe to relevant topics
+mqtt.subscribe(b'image/request')
+print('MQTT OK')
+
+if __name__ == '__main__':
+    print('BOOT OK')
+    while True:
+        mqtt.wait_msg()
